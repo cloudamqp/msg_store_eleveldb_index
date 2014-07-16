1984c2582f42832a74878d14c7a7da805a6ec430
Carl Hörberg <carl.hoerberg@gmail.com>
2014-07-16 13:54:25 +0200

    eleveldb corrections, still wip

--- src/msg_store_eleveldb_index.erl
+++ src/msg_store_eleveldb_index.erl
@@ -12,10 +12,10 @@
          lookup/2, insert/2, update/2, update_fields/3, delete/2,
          delete_object/2, delete_by_file/2, terminate/1]).
 
--include_lib("rabbit_common/include/rabbit_msg_store_index.hrl").
--include_lib("eleveldb/include/eleveldb.hrl").
+-include_lib("rabbit_common/include/rabbit_msg_store.hrl").
+%-include_lib("eleveldb/include/eleveldb.hrl").
 
--define(ELEVELDB_DIR, "eleveldb_data").
+-define(ELEVELDB_DIR, "eleveldb_msg_index").
 
 new(Dir) ->
   Path = get_path(Dir),
@@ -36,25 +36,25 @@ init(Dir) ->
     Ref.
 
 %% Key is MsgId which is binary already
-lookup(Key, Eleveldb) ->
-  case eleveldb:get(Eleveldb, Key) of
+lookup(Key, Ref) ->
+  case eleveldb:get(Ref, Key) of
     {ok, Value} -> #msg_location{} = binary_to_term(Value);
     _ -> not_found
   end.
 
-insert(Obj = #msg_location{ msg_id = MsgId }, Eleveldb) ->
-  ok = eleveldb:put(Eleveldb, MsgId, term_to_binary(Obj)),
+insert(Obj = #msg_location{ msg_id = MsgId }, Ref) ->
+  ok = eleveldb:put(Ref, [{put, MsgId, term_to_binary(Obj)}]),
   ok.
 
-update(Obj = #msg_location{ msg_id = MsgId }, Eleveldb) ->
-  ok = eleveldb:put(Eleveldb, MsgId, term_to_binary(Obj)),
+update(Obj = #msg_location{ msg_id = MsgId }, Ref) ->
+  ok = eleveldb:write(Ref, [{put, MsgId, term_to_binary(Obj)}]),
   ok.
 
 update_fun({Position, NewValue}, ObjAcc) ->
   setelement(Position, ObjAcc, NewValue).
 
-update_fields(Key, Updates, Eleveldb) ->
-  case eleveldb:get(Eleveldb, Key) of
+update_fields(Key, Updates, Ref) ->
+  case eleveldb:get(Ref, Key) of
     {ok, Value} ->
       Obj = #msg_location{} = binary_to_term(Value),
       NewObj =
@@ -62,22 +62,22 @@ update_fields(Key, Updates, Eleveldb) ->
         true -> lists:foldl(fun update_fun/2, Obj, Updates);
         false -> update_fun(Updates, Obj)
       end,
-      ok = eleveldb:put(Eleveldb, Key, term_to_binary(NewObj)),
+      ok = eleveldb:write(Ref, [{put, Key, term_to_binary(NewObj)}]),
       ok;
     _ -> not_found
   end,
   ok.
 
-delete(Key, Eleveldb) ->
-  ok = eleveldb:delete(Eleveldb, Key),
+delete(Key, Ref) ->
+  ok = eleveldb:write(Ref, [{delete, Key}]),
   ok.
 
-delete_object(Obj = #msg_location{ msg_id = MsgId }, Eleveldb) ->
-  case eleveldb:get(Eleveldb, MsgId) of
+delete_object(Obj = #msg_location{ msg_id = MsgId }, Ref) ->
+  case eleveldb:get(Ref, MsgId) of
     {ok, Value} ->
       case Obj =:= binary_to_term(Value) of
         true ->
-          ok = eleveldb:delete(Eleveldb, MsgId),
+          ok = eleveldb:write(Ref, [{delete, MsgId}]),
           ok;
         _ ->
           not_found
@@ -85,12 +85,12 @@ delete_object(Obj = #msg_location{ msg_id = MsgId }, Eleveldb) ->
     _ -> not_found
   end.
 
-delete_by_file(File, Eleveldb) ->
-  ok = eleveldb:fold(Eleveldb,
+delete_by_file(File, Ref) ->
+  ok = eleveldb:fold(Ref,
                      fun(Key, Obj, Acc) ->
                          case (binary_to_term(Obj))#msg_location.file of
                            File ->
-                             eleveldb:delete(Eleveldb, Key),
+                             eleveldb:write(Ref, [{delete, Key}]),
                              Acc;
                            _ ->
                              Acc
@@ -98,6 +98,5 @@ delete_by_file(File, Eleveldb) ->
                      end, ok),
   ok.
 
-terminate(Eleveldb) ->
-  ok = eleveldb:close(Eleveldb),
-  ok.
+terminate(Ref) ->
+  eleveldb:close(Ref).
